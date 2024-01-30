31d032568acb9a41cae4bceb78a92ec48c4383b6
Ravichandra k <ravichandrak89@gmail.com>
2024-01-30 17:49:26 +0530

    first commit

---
+++ src/test/java/demoproject/demojenkins/AppTest.java
@@ -0,0 +1,15 @@
+package demoproject.demojenkins;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+       System.out.println("hello world");
+    }
+}
